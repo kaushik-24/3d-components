8d5464089bff9cf6b39cf066287ae917073cc584
kaushik-24 <grg23kaushik@gmail.com>
2025-03-16 11:48:38 +0545

    add sections

---
+++ app/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <div className='text-3xl '>
+    Hero
+    </div>
+    </>
+  )
+}
+
+export default Hero
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Model from "./components/model";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <>
+    <NavBar />
+    <Hero />
+    <Model /> 
+    <Footer />
     </>
   ) 
 }

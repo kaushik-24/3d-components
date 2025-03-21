c3cc00228e229080364b52fd951a73cc54442364
kaushik-24 <grg23kaushik@gmail.com>
2025-03-21 08:25:09 +0545

    add fonts to hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Kavoon } from 'next/font/google'
 
 const kavoon = Kavoon({
+  subsets: ["latin"],
   weight: "400",
 })
 
@@ -10,7 +11,7 @@ const Hero = () => {
     <>
     <section className='mt-[36vh]'>
     <div className=' flex flex-col justify-center items-center '>
-    <div className={`text-7xl md:text-[14vh] lg:text-[18vh]`}>3<span className='text-[#901430] font-extrabold'>D</span>ee
+    <div className={`${kavoon.className} text-7xl md:text-[14vh] lg:text-[18vh]`}>3<span className='text-[#901430] font-extrabold'>D</span>ee
     </div>
     <p className='m-6'>
     This is just a page to showcase my 3D models. Ready-to-implement using three.js.
